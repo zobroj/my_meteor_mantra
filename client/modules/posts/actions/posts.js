79591edd8f352b4738d00d186e3d85aec96423d8
VonIobro <jonathan.zobro@gmail.com>
2016-06-12 17:54:17 +0800

    fix typo on posts

--- client/modules/posts/actions/posts.js
+++ client/modules/posts/actions/posts.js
@@ -11,7 +11,7 @@ export default {
     const id = Meteor.uuid();
     // There is a method stub for this in the config/method_stubs
     // That's how we are doing latency compensation
-    Meteor.call( 'post.create', id, author, title, content, (err) => {
+    Meteor.call( 'posts.create', id, author, title, content, (err) => {
       if (err) {
         return LocalState.set( 'SAVING_ERROR', err.message );
       }
