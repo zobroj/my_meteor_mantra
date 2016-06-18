a72f39caf2065763191c2425132fb9b1ab5ff19d
VonIobro <jonathan.zobro@gmail.com>
2016-06-18 19:06:15 +0700

    use states in the same Class

--- client/modules/_dev/comment_box.jsx
+++ client/modules/_dev/comment_box.jsx
@@ -4,21 +4,12 @@ import CommentList from './comment_list';
 import CommentForm from './comment_form';
 
 const CommentBox = React.createClass({
-  getInitialState: function() {
-    return {
-      data: [
-        { id: 1, author: "Pete Hunt", text: "This is one comment" },
-        { id: 2, author: "Jordan Walke", text: "This is *another* comment" },
-        { id: 3, author: "Bilbo Baggins", text: "Lord of the Rings character" },
-        { id: 4, author: "Alice In Chains", text: "Man in a Hole" }
-      ]
-    }
-  },
+
   render: function() {
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList data={ this.state.data }/>
+        <CommentList />
         <CommentForm />
       </div>
     );
--- client/modules/_dev/comment_list.jsx
+++ client/modules/_dev/comment_list.jsx
@@ -1,24 +1,31 @@
 import React from 'react';
 import Comment from './comment';
 
-class CommentList extends React.Component {
-  render() {
-
-    const commentNodes = this.props.data.map( ( comment ) => {
+const CommentList = React.createClass({
+  getInitialState: function() {
+    return {
+      data: [
+        { id: 1, author: "Pete Hunt", text: "This is one comment" },
+        { id: 2, author: "Jordan Walke", text: "This is *another* comment" },
+        { id: 3, author: "Bilbo Baggins", text: "Lord of the Rings character" },
+        { id: 4, author: "Alice In Chains", text: "Man in a Hole" }
+      ]
+    }
+  },
+  render: function() {
+    const commentNodes = this.state.data.map( ( comment ) => {
       return (
         <Comment author={ comment.author } key={ comment.id }>
           { comment.text }
         </Comment>
       )
     })
-
     return (
       <div className="commentList">
         { commentNodes }
       </div>
     )
-
   }
-}
+});
 
 export default CommentList;
