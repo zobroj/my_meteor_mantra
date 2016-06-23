791d040feb56fad31dbdb8ae56b3fe1c06b445ce
VonIobro <jonathan.zobro@gmail.com>
2016-06-23 14:50:58 +0700

    refactor for code-style

--- client/configs/components.js
+++ client/configs/components.js
@@ -1,6 +1,6 @@
-import MainLayout from '/client/modules/app/layouts/main_layout.jsx';
-import ListLayout from '/client/modules/app/layouts/list_layout.jsx';
-import AccountNotLoggedIn from '/client/modules/app/components/account_not_logged_in.jsx';
+import MainLayout from '/client/modules/app/layouts/main_layout';
+import ListLayout from '/client/modules/app/layouts/list_layout';
+import AccountNotLoggedIn from '/client/modules/app/components/account_not_logged_in';
 
 export {
   MainLayout,
