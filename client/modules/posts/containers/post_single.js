e5ed6f92c97898723932a2d1496ce5530656cc6f
VonIobro <jonathan.zobro@gmail.com>
2016-06-24 17:53:43 +0700

    refactor post-single load logic and add LocalState error

--- client/modules/posts/containers/post_single.js
+++ client/modules/posts/containers/post_single.js
@@ -3,28 +3,28 @@ import { useDeps, composeWithTracker, composeAll } from 'mantra-core'
 
 export const composer = ( { context, postId }, onData ) => {
 
-  const { Meteor, Collections, FlowRouter } = context()
+  const { Meteor, Collections, FlowRouter, LocalState } = context()
+  const error = LocalState.get( 'POSTS_ERROR' )
 
   if ( Meteor.subscribe( 'posts.single', postId ).ready() ) {
-
-    const post = Collections.Posts.findOne( postId )
-    onData( null, { post } )
-
-  } else {
 
     const post = Collections.Posts.findOne( postId )
 
     if ( post ) {
       onData( null, { post } )
     } else {
-      onData()
+
+      LocalState.set( 'POSTS_ERROR', 'Post could not be found. Please try again.' )
       console.log('post not found')
-      FlowRouter.go('/post/notfound')
+      FlowRouter.go('/post/notFound')
+      // FlowRouter.path('/posts', { error } )
     }
   }
+
+  return LocalState.set( 'POSTS_ERROR', null )
 }
 
 export default composeAll(
   composeWithTracker( composer ),
-  useDeps()
+  useDeps(),
 )( PostSingle )
