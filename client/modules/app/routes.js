3551c58fd6c658069f42415af0d272aed5947648
VonIobro <jonathan.zobro@gmail.com>
2016-06-24 12:20:45 +0700

    add not found route

--- client/modules/app/routes.js
+++ client/modules/app/routes.js
@@ -1,32 +1,43 @@
 import React from 'react';
-import { mount } from 'react-mounter';
+import { mount } from 'react-mounter'
 
 import {
+  MainLayout,
   SplitLayout,
-} from '/client/configs/components';
+} from '/client/configs/components'
+import AppNotFound from './components/app_not_found'
 
-import AccountRegister from './containers/account_register';
-import AccountLogin from './containers/account_login';
+import AccountRegister from './containers/account_register'
+import AccountLogin from './containers/account_login'
 
 export default function ( injectDeps, { FlowRouter } ) {
 
-  const SplitLayoutCtx = injectDeps ( SplitLayout );
+  const MainLayoutCtx = injectDeps ( MainLayout )
+  const SplitLayoutCtx = injectDeps ( SplitLayout )
 
   FlowRouter.route( '/register', {
     name: "accounts.register",
     action() {
       mount( SplitLayoutCtx, {
         content: () => ( <AccountRegister /> ),
-      });
+      })
     }
-  });
+  })
 
   FlowRouter.route( '/login', {
     name: "accounts.login",
     action() {
       mount( SplitLayoutCtx, {
         content: () => ( <AccountLogin /> ),
-      });
+      })
     }
-  });
+  })
+
+  FlowRouter.notFound = {
+    action() {
+      mount( MainLayoutCtx, {
+        content: () => ( <AppNotFound /> ),
+      })
+    }
+  }
 }
