6eb1c6ce850acccdd6989edb29180dfb197bf25e
VonIobro <jonathan.zobro@gmail.com>
2016-06-27 20:10:28 +0700

    send email with ssr

--- server/methods/emails.js
+++ server/methods/emails.js
@@ -8,11 +8,21 @@ export default function () {
 
       if ( userId ) {
 
+        SSR.compileTemplate(
+          'htmlEmail',
+          Assets.getText( 'html-email.html')
+        )
+
+        let emailData = {
+          username: "VonIobro",
+          email: "von.iobro@gmail.com",
+        }
+
         Email.send({
           to: "von.iobro@gmail.com",
           from: "abctutorme123@gmail.com",
           subject: "My Mantra Signup",
-          text: "Thanks for signing up^^",
+          html: SSR.render( 'htmlEmail', emailData ),
         })
 
         // return Accounts.sendVerificationEmail( userId )
