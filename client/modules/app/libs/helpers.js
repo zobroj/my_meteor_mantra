bfd79cdea2b948ce07a068ef561269a2d9bea04b
VonIobro <jonathan.zobro@gmail.com>
2016-06-28 01:24:12 +0700

    Handle if route name i undefined

--- client/modules/app/libs/helpers.js
+++ client/modules/app/libs/helpers.js
@@ -1,6 +1,19 @@
 export function isActiveRoute( compareRoute ) {
-  // remove characters after '.' in route name
-  let currentRoute = FlowRouter.getRouteName().split( ".", 1 )
 
-  return currentRoute == compareRoute ? 'active' : ''
+  let currentRoute = FlowRouter.getRouteName()
+
+  // if user goes to route w/o
+  if ( currentRoute != undefined ) {
+
+    // remove characters after '.' in route name
+    let _currentRoute = FlowRouter.getRouteName().split( ".", 1 )
+
+    return _currentRoute == compareRoute ? 'active' : ''
+
+  } else {
+
+    return '';
+    
+  }
+
 }
