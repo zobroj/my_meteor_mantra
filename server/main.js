3824627d02c6c5b1f0ae91e6118f97ec096fd924
VonIobro <jonathan.zobro@gmail.com>
2016-06-28 23:07:20 +0700

    comment out email commands, email doesn't work unless configured 

--- server/main.js
+++ server/main.js
@@ -3,11 +3,13 @@ import methods from './methods'
 import addInitialData from './configs/initial_adds.js'
 import accountsConfig from './configs/accounts_config'
 
-// create this file and put smtp email settings here:
-import private_credentials from './configs/private_credentials'
-
 publications()
 methods()
 addInitialData()
 accountsConfig()
+
+// uncomment below to enable emails, create file and put in smtp settings:
+/*
+import private_credentials from './configs/private_credentials'
 private_credentials()
+*/
