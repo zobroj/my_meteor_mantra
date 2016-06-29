b1d955869dc761b06586ea1d9e60a1a4a5b6c156
VonIobro <jonathan.zobro@gmail.com>
2016-06-29 17:11:38 +0700

    rename app module to core module

--- client/main.js
+++ client/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'mantra-core';
 import initContext from './configs/context';
 
 // modules
-import appModule from './modules/core';
+import coreModule from './modules/core';
 import mainModule from './modules/main';
 import postsModule from './modules/posts';
 import commentsModule from './modules/comments';
@@ -16,7 +16,7 @@ const context = initContext();
 
 // create app
 const app = createApp( context );
-app.loadModule( appModule );
+app.loadModule( coreModule );
 app.loadModule( mainModule );
 app.loadModule( postsModule );
 app.loadModule( commentsModule );
