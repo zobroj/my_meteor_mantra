a2e9d2ded0e5ac059f400c3f12bd288d9d809741
VonIobro <jonathan.zobro@gmail.com>
2016-07-02 02:52:18 +0700

    refactor for react.bootstrap and remove refs

--- client/modules/core/components/account_login.jsx
+++ client/modules/core/components/account_login.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
 import AccountLoggedIn from './account_logged_in'
 import { AppLoading } from '/client/configs/components'
+import {
+  FormGroup,
+  ControlLabel,
+  FormControl,
+  Button,
+} from 'react-bootstrap'
 
 class AccountLogin extends React.Component {
+  constructor( props ) {
+    super( props )
+    this.state = {
+      email: '',
+      password: '',
+    }
+  }
+
+  handleEmailChange( event ) {
+    this.setState({ email: event.target.value })
+  }
+
+  handlePasswordChange( event ) {
+    this.setState({ password: event.target.value })
+  }
 
   displayUser() {
     return(
@@ -16,31 +37,37 @@ class AccountLogin extends React.Component {
   }
 
   displayGuest() {
-
     const { error } = this.props
-
     return (
-      <form role="form" id="login-form" onSubmit={ this._login.bind( this ) }>
+      <form onSubmit={ this._login.bind( this ) }>
         { error ? <p style={ { color: 'red' } }>{ error }</p> : null }
         <h3>Log In to Your Account</h3>
-        <div className="form-group">
-          <label for="email">Email Address</label>
-          <input className="form-control" type="email" ref="email" placeholder="Enter email"/>
-        </div>
-        <div className="form-group">
-          <label for="password">Password</label>
-          <input className="form-control" type="password" ref="password" placeholder="Enter password"/>
-        </div>
-        <button type="submit" className="btn btn-default">Log In</button>
+        <FormGroup>
+          <ControlLabel>Email Address</ControlLabel>
+          <FormControl
+            type="email"
+            placeholder="Enter email"
+            value={ this.state.email }
+            onChange={ this.handleEmailChange.bind( this ) }
+          />
+        </FormGroup>
+        <FormGroup>
+          <ControlLabel>Password</ControlLabel>
+          <FormControl
+            type="password"
+            placeholder="Enter password"
+            value={ this.state.pasword }
+            onChange={ this.handlePasswordChange.bind( this ) }
+          />
+        </FormGroup>
+        <Button type="submit" >Log In</Button>
       </form>
     )
   }
 
   render() {
     const { loggedIn, loggingIn } = this.props
-
     if ( loggingIn ) { return this.displayLoading() }
-
     return (
       <div>
       { loggedIn ? this.displayUser() : this.displayGuest() }
@@ -51,8 +78,8 @@ class AccountLogin extends React.Component {
   _login( event ) {
     event.preventDefault()
     const { login } = this.props
-    const { email, password } = this.refs
-    login( email.value, password.value )
+    const { email, password } = this.state
+    login( email, password )
   }
 
 }
