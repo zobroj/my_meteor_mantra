7c02a31b4705037a55f36908b426805e8aa8aa4d
VonIobro <jonathan.zobro@gmail.com>
2016-07-06 15:06:21 +0700

    birdpoop

--- client/main.js
+++ client/main.js
@@ -1,26 +1,26 @@
-import { createApp } from 'mantra-core';
-import initContext from './configs/context';
+import { createApp } from 'mantra-core'
+import initContext from './configs/context'
 
 // modules
-import coreModule from './modules/core';
-import mainModule from './modules/main';
-import postsModule from './modules/posts';
-import commentsModule from './modules/comments';
+import coreModule from './modules/core'
+import mainModule from './modules/main'
+import postsModule from './modules/posts'
+import commentsModule from './modules/comments'
 import moduleTemplate from './modules/_module_template'
 import reactBootstrapModule from './modules/react_bootstrap'
 
 // import bootstrap.js plugin
-import 'bootstrap-sass';
+import 'bootstrap-sass'
 
 // init context
-const context = initContext();
+const context = initContext()
 
 // create app
-const app = createApp( context );
-app.loadModule( coreModule );
-app.loadModule( mainModule );
-app.loadModule( postsModule );
-app.loadModule( commentsModule );
-app.loadModule( moduleTemplate );
-app.loadModule( reactBootstrapModule );
-app.init();
+const app = createApp( context )
+app.loadModule( coreModule )
+app.loadModule( mainModule )
+app.loadModule( postsModule )
+app.loadModule( commentsModule )
+app.loadModule( moduleTemplate )
+app.loadModule( reactBootstrapModule )
+app.init()
--- client/modules/react_bootstrap/containers/react_bootstrap.js
+++ client/modules/react_bootstrap/containers/react_bootstrap.js
@@ -1,13 +1,13 @@
-import { useDeps, composeWithTracker, composeAll } from 'mantra-core';
-import Component from '../components/react_bootstrap.jsx';
-import { authComposer } from 'meteor-auth';
+import { useDeps, composeWithTracker, composeAll } from 'mantra-core'
+import Component from '../components/react_bootstrap.jsx'
+import { authComposer } from 'meteor-auth'
 
 export const composer = ( { context }, onData ) => {
-  onData( null, { } );
-};
+  onData( null, { } )
+}
 
 export default composeAll(
   composeWithTracker( composer ),
   composeWithTracker( authComposer ),
   useDeps()
-)( Component );
+)( Component )
