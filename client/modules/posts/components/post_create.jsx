68998fe1f8464e1c91ba2a151b5c2088e700a257
VonIobro <jonathan.zobro@gmail.com>
2016-07-25 00:12:33 +0700

    airbnb

--- client/modules/posts/components/post_create.jsx
+++ client/modules/posts/components/post_create.jsx
@@ -1,41 +1,36 @@
-import React from 'react'
-import { AppErrorMsg, AppLoading } from '/client/configs/components'
-import {
-  Button,
-  ControlLabel,
-  FormControl,
-  FormGroup,
-} from 'react-bootstrap'
+import React from 'react';
+import { AppErrorMsg, AppLoading } from '/client/configs/components';
+import { Button, ControlLabel, FormControl, FormGroup } from 'react-bootstrap';
 
 class PostCreate extends React.Component {
-  constructor( props ) {
-    super( props )
+  constructor(props) {
+    super(props);
     this.state = {
       title: '',
       content: '',
-    }
-    this.handleTitleChange = this.handleTitleChange.bind( this )
-    this.handleContentChange = this.handleContentChange.bind( this )
-    this._createPost = this._createPost.bind( this )
+    };
+    this.handleTitleChange = this.handleTitleChange.bind(this);
+    this.handleContentChange = this.handleContentChange.bind(this);
+    this._createPost = this._createPost.bind(this);
   }
-  handleTitleChange( event ) {
-    this.setState({ title: event.target.value })
+  handleTitleChange(event) {
+    this.setState({ title: event.target.value });
   }
-  handleContentChange( event ) {
-    this.setState({ content: event.target.value })
+  handleContentChange(event) {
+    this.setState({ content: event.target.value });
   }
   displayUser() {
-    const { error } = this.props
+    const { error } = this.props;
     return (
-      <form onSubmit={ this._createPost }>
-        <AppErrorMsg error={ error } />
+      <form onSubmit={this._createPost}>
+        <AppErrorMsg error={error} />
         <FormGroup>
           <ControlLabel>Title</ControlLabel>
           <FormControl
             type="text"
             placeholder="Give a short title to your post."
-            value={ this.state.title }
-            onChange={ this.handleTitleChange }
+            value={this.state.title}
+            onChange={this.handleTitleChange}
           />
         </FormGroup>
         <FormGroup>
@@ -43,51 +38,56 @@ class PostCreate extends React.Component {
           <FormControl
             type="text"
             placeholder="Something interesting..."
-            value={ this.state.content }
-            onChange={ this.handleContentChange }
+            value={this.state.content}
+            onChange={this.handleContentChange}
           />
         </FormGroup>
         <Button type="submit">New Post</Button>
       </form>
-    )
+    );
   }
   displayGuest() {
     return (
       <div>
         <p>Log in, or create an account to add a conversation.</p>
       </div>
-    )
+    );
   }
   displayLoading() {
     return (
       <AppLoading />
-    )
+    );
   }
-  render() {
-    const { loggedIn, loggingIn } = this.props
-    const { emailVerified } = this.props.Users()
-    if ( loggingIn ) { return this.displayLoading() }
-    if ( loggedIn && !emailVerified ) {
-      return (
-        <div>This functionality is not available to unverfied users.</div>
-      )
-    }
-    if ( loggedIn ) {
-      return this.displayUser()
-    } else {
-      return this.displayGuest()
-    }
-  }
-  _createPost( event ) {
-    event.preventDefault()
-    const { create } = this.props
-    const { title, content } = this.state
-    create( title, content )
+  _createPost(event) {
+    event.preventDefault();
+    const { create } = this.props;
+    const { title, content } = this.state;
+    create(title, content);
     this.setState({
       title: '',
       content: '',
-    })
+    });
+  }
+  render() {
+    const { loggedIn, loggingIn } = this.props;
+    const { emailVerified } = this.props.Users();
+    if (loggingIn) { return this.displayLoading(); }
+    if (loggedIn && !emailVerified) {
+      return (
+        <div>This functionality is not available to unverfied users.</div>
+      );
+    }
+    if (loggedIn) { return this.displayUser(); }
+    return this.displayGuest();
   }
 }
 
-export default PostCreate
+export default PostCreate;
+
+PostCreate.propTypes = {
+  loggedIn: React.PropTypes.bool,
+  error: React.PropTypes.string,
+  loggingIn: React.PropTypes.bool,
+  emailVerified: React.PropTypes.bool,
+  create: React.PropTypes.func,
+};
