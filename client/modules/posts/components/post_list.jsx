d12d8ab1e95a8bff97d4f8ccae3d752e41e6b3ba
VonIobro <jonathan.zobro@gmail.com>
2016-07-25 02:11:49 +0700

    airbnb: posts to stateless

--- client/modules/posts/components/post_list.jsx
+++ client/modules/posts/components/post_list.jsx
@@ -3,33 +3,33 @@ import PostCreate from '../containers/post_create.js';
 import { AppErrorMsg } from '/client/configs/components';
 import { Col, ListGroup, ListGroupItem, Panel, Row } from 'react-bootstrap';
 
-export default class PostList extends React.Component {
-  render() {
-    const postNodes = this.props.posts.map(post => (
-      <ListGroupItem key={post._id}>
-        <a href={`/post/${post._id}`}>{post.title}</a>
-        <p className="small">
-          <strong>{post.author}</strong> - {post.createdAt.toLocaleDateString()}
-        </p>
-      </ListGroupItem>
-    ));
-    return (
-      <Row>
-        <AppErrorMsg error={this.props.error} />
-        <Col md={12}>
-          <Panel header="Posts">
-            <ListGroup fill>
-              {postNodes}
-            </ListGroup>
-          </Panel>
-          <Panel header="Create a post">
-            <PostCreate />
-          </Panel>
-        </Col>
-      </Row>
-    );
-  }
-}
+const PostList = ({ posts, error }) => {
+  const postNodes = posts.map(post => (
+    <ListGroupItem key={post._id}>
+      <a href={`/post/${post._id}`}>{post.title}</a>
+      <p className="small">
+        <strong>{post.author}</strong> - {post.createdAt.toLocaleDateString()}
+      </p>
+    </ListGroupItem>
+  ));
+  return (
+    <Row>
+      <AppErrorMsg error={error} />
+      <Col md={12}>
+        <Panel header="Posts">
+          <ListGroup fill>
+            {postNodes}
+          </ListGroup>
+        </Panel>
+        <Panel header="Create a post">
+          <PostCreate />
+        </Panel>
+      </Col>
+    </Row>
+  );
+};
+
+export default PostList;
 
 PostList.propTypes = {
   error: React.PropTypes.string,
--- client/modules/posts/routes.js
+++ client/modules/posts/routes.js
@@ -1,36 +1,28 @@
 import React from 'react';
 import { mount } from 'react-mounter';
-
-import {
-  LayoutList,
-  AppNotFound,
-} from '/client/configs/components';
-
+import { LayoutList } from '/client/configs/components';
 import PostList from './containers/post_list';
 import PostSingle from './containers/post_single';
 
-export default function ( injectDeps, { FlowRouter } ) {
-
-  const LayoutListCtx = injectDeps( LayoutList );
+export default function (injectDeps, { FlowRouter }) {
+  const LayoutListCtx = injectDeps(LayoutList);
 
   // Overview of all conversations
-  FlowRouter.route( '/post', {
-    name: "posts.list",
+  FlowRouter.route('/post', {
+    name: 'posts.list',
     action() {
-      mount( LayoutListCtx, {
-        content: () => ( <PostList /> ),
+      mount(LayoutListCtx, {
+        content: () => (<PostList />),
       });
-    }
+    },
   });
-
   // Single conversation view
-  FlowRouter.route( '/post/:postId', {
-    name: "posts.single",
-    action( { postId } ) {
-      mount( LayoutListCtx, {
-        content: ()=> ( <PostSingle postId={ postId } /> ),
+  FlowRouter.route('/post/:postId', {
+    name: 'posts.single',
+    action({ postId }) {
+      mount(LayoutListCtx, {
+        content: () => (<PostSingle postId={postId} />),
       });
-    }
+    },
   });
-
 }
