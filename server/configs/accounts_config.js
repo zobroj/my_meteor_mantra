871b2cb30504305c1964213a7ee3a4f1e7fd477a
VonIobro <jonathan.zobro@gmail.com>
2016-07-25 00:57:33 +0700

    few more linting errors

--- server/configs/accounts_config.js
+++ server/configs/accounts_config.js
@@ -1,11 +1,11 @@
 // server configs
 export default function () {
   // call this to add non-standard Account fields
-  Accounts.onCreateUser(function (options, user)  {
-    console.log("server onCreateUser called");
-    console.log("user.username: " + user.username);
+  Accounts.onCreateUser((options, user) => {
+    console.log('server onCreateUser called');
+    console.log(`user.username: ${user.username}`);
     // check for unique username?
-    return user
+    return user;
   });
 
   Accounts.emailTemplates.siteName = 'My Meteor Mantra';
