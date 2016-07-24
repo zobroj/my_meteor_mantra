4eb04d46e74c95ea8695a1ee46b4194f021ae6fb
VonIobro <jonathan.zobro@gmail.com>
2016-07-25 00:11:56 +0700

    airbnb

--- client/modules/posts/actions/posts.js
+++ client/modules/posts/actions/posts.js
@@ -1,32 +1,25 @@
 // actions
 export default {
-
-  create( { Meteor, LocalState, FlowRouter }, title, content ) {
-
-    if ( !title || !content ) {
-      return LocalState.set( 'POSTS_CREATE_ERROR', 'Title & Content are required!' )
+  create({ Meteor, LocalState, FlowRouter }, title, content) {
+    if (!title || !content) {
+      return LocalState.set('POSTS_CREATE_ERROR', 'Title & Content are required!');
     }
 
-    LocalState.set( 'POSTS_CREATE_ERROR', null )
+    LocalState.set('POSTS_CREATE_ERROR', null);
 
-    const _id = Meteor.uuid()
-    const userId = Meteor.userId()
-    const author = Meteor.user().username
-    console.log("author is " + author)
-    // There is a method stub for this in the config/method_stubs
-    // That's how we are doing latency compensation
-    Meteor.call( 'posts.create', _id, userId, author, title, content, (err) => {
+    const _id = Meteor.uuid();
+    const userId = Meteor.userId();
+    const author = Meteor.user().username;
+
+    Meteor.call('posts.create', _id, userId, author, title, content, (err) => {
       if (err) {
-        return LocalState.set( 'POSTS_CREATE_ERROR', err.message )
+        return LocalState.set('POSTS_CREATE_ERROR', err.message);
       }
-    })
+    });
 
-    FlowRouter.go( `/post/${ _id }` )
-
+    FlowRouter.go(`/post/${_id}`);
   },
-
-  clearErrors( { LocalState }, errorState ) {
-    return LocalState.set( errorState, null )
+  clearErrors({ LocalState }, errorState) {
+    return LocalState.set(errorState, null);
   },
-
-}
+};
