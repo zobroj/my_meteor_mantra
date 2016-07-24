469c1aa5b8a60b24b7309207780bebf3faedce67
VonIobro <jonathan.zobro@gmail.com>
2016-07-25 00:57:46 +0700

    update custom rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,5 +11,7 @@ module.exports = {
   "rules": {
     // override defaults for rule from base configurations
     "import/no-unresolved": 0,
+    "no-undef": 0, // Mantra imports a lot of files automagically
+    "max-len": 0, // Sometimes you just gotta be verbose
   }
 }
