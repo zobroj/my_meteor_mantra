bf2f35aee3e542ef6a41920c42dd14fc5cae1f85
VonIobro <jonathan.zobro@gmail.com>
2016-07-31 20:15:32 +0700

    return auth to preferences

--- client/modules/core/containers/account_preferences.js
+++ client/modules/core/containers/account_preferences.js
@@ -1,5 +1,6 @@
-import { useDeps, composeAll } from 'mantra-core';
+import { useDeps, composeAll, composeWithTracker } from 'mantra-core';
 import Component from '../components/account_preferences';
+import { authComposer } from '/client/configs/composers';
 
 export const depsMapper = (context, actions) => ({
   deleteAccount: actions.accounts.deleteAccount,
@@ -8,5 +9,6 @@ export const depsMapper = (context, actions) => ({
 });
 
 export default composeAll(
+  composeWithTracker(authComposer),
   useDeps(depsMapper)
 )(Component);
