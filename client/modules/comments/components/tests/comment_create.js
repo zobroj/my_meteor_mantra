d75ec60a7925083a52f8062e54148b225329a997
VonIobro <jonathan.zobro@gmail.com>
2016-08-08 22:34:34 +0700

    hmmm

--- client/modules/comments/components/tests/comment_create.js
+++ client/modules/comments/components/tests/comment_create.js
@@ -1,25 +1,35 @@
-/*
-const {describe, it} = global;
-import {expect} from 'chai';
-import {shallow} from 'enzyme';
-import CreateComment from '../comment_create';
+const { describe, it } = global;
+import { expect } from 'chai';
+import { mount, shallow } from 'enzyme';
+import CommentCreate from '../comment_create';
+import AppErrorMsg from '../../../core/components/app_error_msg';
 
 describe('comments.components.comment_create', () => {
+  it('contains an <AppErrorMsg/> component', () => {
+    const wrapper = shallow(<CommentCreate loggedIn={true} />);
+    expect(wrapper.find(AppErrorMsg)).to.have.length(1);
+    wrapper.debug();
+  });
+  /* TODO move this to AppErrorMsg?
   it('should show the error if there are any', () => {
     const error = 'TheError';
     const el = shallow(<CommentCreate error={error} />);
     expect(el.html()).to.match(/TheError/);
   });
+  */
 
+  /*
   it('should display the create comment form', () => {
-    const el = shallow(<CommentCreate />);
+    const el = shallow(<CommentCreate loggedIn="true" />);
     const textarea = el.find('textarea').first();
     const button = el.find('button').first();
 
     expect(textarea.node.ref).to.be.equal('text');
     expect(button.prop('onClick')).to.be.a('function');
   });
+  */
 
+/*
   it('should create a new comment when click on the button', done => {
     const postId = 'the-id';
     const text = 'the-text';
@@ -39,5 +49,5 @@ describe('comments.components.comment_create', () => {
 
     el.find('button').simulate('click');
   });
+  */
 });
-*/
