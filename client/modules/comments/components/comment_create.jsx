5b59a6e1f609ee2eaeb3c28394d00d83a3f005ef
VonIobro <jonathan.zobro@gmail.com>
2016-08-09 00:32:44 +0700

    CommentCreate uses AuthCheck

--- client/modules/comments/components/comment_create.jsx
+++ client/modules/comments/components/comment_create.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AppErrorMsg, AppLoading } from '/client/configs/components';
+import { AppErrorMsg, AuthCheck } from '/client/configs/components';
 import { Button, FormControl, FormGroup } from 'react-bootstrap';
 
 class CommentCreate extends React.Component {
@@ -14,35 +14,6 @@ class CommentCreate extends React.Component {
   handleCommentChange(event) {
     this.setState({ comment: event.target.value });
   }
-  displayUser() {
-    const { error } = this.props;
-    return (
-      <form onSubmit={this._createComment}>
-        <AppErrorMsg error={error} />
-        <FormGroup>
-          <FormControl
-            type="text"
-            placeholder="Your comment...."
-            value={this.state.comment}
-            onChange={this.handleCommentChange}
-          />
-        </FormGroup>
-        <Button type="submit">Add Comment</Button>
-      </form>
-    );
-  }
-  displayGuest() {
-    return (
-      <div>
-        <p>To add a comment, log in or create an account.</p>
-      </div>
-    );
-  }
-  displayLoading() {
-    return (
-      <AppLoading />
-    );
-  }
   _createComment(event) {
     event.preventDefault();
     const { create, postId, user } = this.props;
@@ -51,17 +22,23 @@ class CommentCreate extends React.Component {
     this.setState({ comment: '' });
   }
   render() {
-    const { loggedIn, loggingIn, emailVerified } = this.props;
-    if (loggingIn) { return this.displayLoading(); }
-    if (loggedIn && !emailVerified) {
-      return (
-        <div>This functionality is not available to unverfied users.</div>
-      );
-    }
-    if (loggedIn) {
-      return this.displayUser();
-    }
-    return this.displayGuest();
+    const { error } = this.props;
+    return (
+      <AuthCheck>
+        <form onSubmit={this._createComment}>
+          <AppErrorMsg error={error} />
+          <FormGroup>
+            <FormControl
+              type="text"
+              placeholder="Your comment...."
+              value={this.state.comment}
+              onChange={this.handleCommentChange}
+            />
+          </FormGroup>
+          <Button type="submit">Add Comment</Button>
+        </form>
+      </AuthCheck>
+    );
   }
 }
 
@@ -71,8 +48,5 @@ CommentCreate.propTypes = {
   create: React.PropTypes.func,
   postId: React.PropTypes.string,
   error: React.PropTypes.string,
-  emailVerified: React.PropTypes.bool,
-  loggingIn: React.PropTypes.bool,
-  loggedIn: React.PropTypes.bool,
   user: React.PropTypes.object,
 };
