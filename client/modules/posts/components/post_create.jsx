989eb5fe2cde68a370b7ade4dc0d34b29587b5e0
VonIobro <jonathan.zobro@gmail.com>
2016-08-09 00:27:47 +0700

    PostCreate uses AuthCheck

--- client/modules/posts/components/post_create.jsx
+++ client/modules/posts/components/post_create.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AppErrorMsg, AppLoading } from '/client/configs/components';
+import { AppErrorMsg, AuthCheck } from '/client/configs/components';
 import { Button, ControlLabel, FormControl, FormGroup } from 'react-bootstrap';
 
 class PostCreate extends React.Component {
@@ -19,45 +19,6 @@ class PostCreate extends React.Component {
   handleContentChange(event) {
     this.setState({ text: event.target.value });
   }
-  displayUser() {
-    const { error } = this.props;
-    return (
-      <form onSubmit={this._createPost}>
-        <AppErrorMsg error={error} />
-        <FormGroup>
-          <ControlLabel>Title</ControlLabel>
-          <FormControl
-            type="text"
-            placeholder="Give a short title to your post."
-            value={this.state.title}
-            onChange={this.handleTitleChange}
-          />
-        </FormGroup>
-        <FormGroup>
-          <ControlLabel>Content</ControlLabel>
-          <FormControl
-            type="text"
-            placeholder="Something interesting..."
-            value={this.state.text}
-            onChange={this.handleContentChange}
-          />
-        </FormGroup>
-        <Button type="submit">New Post</Button>
-      </form>
-    );
-  }
-  displayGuest() {
-    return (
-      <div>
-        <p>Log in, or create an account to add a conversation.</p>
-      </div>
-    );
-  }
-  displayLoading() {
-    return (
-      <AppLoading />
-    );
-  }
   _createPost(event) {
     event.preventDefault();
     const { create, user } = this.props;
@@ -69,15 +30,33 @@ class PostCreate extends React.Component {
     });
   }
   render() {
-    const { loggedIn, loggingIn, emailVerified } = this.props;
-    if (loggingIn) { return this.displayLoading(); }
-    if (loggedIn && !emailVerified) {
-      return (
-        <div>This functionality is not available to unverfied users.</div>
-      );
-    }
-    if (loggedIn) { return this.displayUser(); }
-    return this.displayGuest();
+    const { error } = this.props;
+    return (
+      <AuthCheck>
+        <form onSubmit={this._createPost}>
+          <AppErrorMsg error={error} />
+          <FormGroup>
+            <ControlLabel>Title</ControlLabel>
+            <FormControl
+              type="text"
+              placeholder="Give a short title to your post."
+              value={this.state.title}
+              onChange={this.handleTitleChange}
+            />
+          </FormGroup>
+          <FormGroup>
+            <ControlLabel>Content</ControlLabel>
+            <FormControl
+              type="text"
+              placeholder="Something interesting..."
+              value={this.state.text}
+              onChange={this.handleContentChange}
+            />
+          </FormGroup>
+          <Button type="submit">New Post</Button>
+        </form>
+      </AuthCheck>
+    );
   }
 }
 
@@ -86,8 +65,5 @@ export default PostCreate;
 PostCreate.propTypes = {
   create: React.PropTypes.func,
   error: React.PropTypes.string,
-  emailVerified: React.PropTypes.bool,
-  loggedIn: React.PropTypes.bool,
-  loggingIn: React.PropTypes.bool,
   user: React.PropTypes.object,
 };
