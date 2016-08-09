52ee1f724a72f5a81ab9525d25506e6c8cb0fb27
VonIobro <jonathan.zobro@gmail.com>
2016-08-10 00:19:09 +0700

    remove unnecessary imports

--- client/modules/comments/components/tests/comment_create.js
+++ client/modules/comments/components/tests/comment_create.js
@@ -2,19 +2,17 @@ const { describe, it } = global;
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
 import CommentCreate from '../comment_create';
-import { AppErrorMsg } from '/client/configs/components';
-import { Button, FormControl } from 'react-bootstrap';
 
 describe('comments.components.comment_create', () => {
   it('contains an <AppErrorMsg/> component', () => {
     const wrapper = shallow(<CommentCreate />);
-    expect(wrapper.find(AppErrorMsg)).to.have.length(1);
+    expect(wrapper.find('AppErrorMsg')).to.have.length(1);
   });
 
   it('should display the create comment form', () => {
     const wrapper = shallow(<CommentCreate />);
-    const textarea = wrapper.find(FormControl).first();
-    const button = wrapper.find(Button).first();
+    const textarea = wrapper.find('FormControl').first();
+    const button = wrapper.find('Button').first();
 
     expect(textarea.prop('value')).to.be.equal('');
     expect(button.prop('onClick')).to.be.a('function');
@@ -39,7 +37,7 @@ describe('comments.components.comment_create', () => {
     );
     wrapper.setState({ comment });
 
-    const button = wrapper.find(Button).first();
+    const button = wrapper.find('Button').first();
     button.simulate('click');
   });
 
