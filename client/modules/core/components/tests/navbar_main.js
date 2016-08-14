93886be8c4381d1d1ff7c5a35367b2bca039490f
VonIobro <jonathan.zobro@gmail.com>
2016-08-15 04:46:08 +0700

    import react-bootrap as component dependency

--- client/modules/core/components/tests/navbar_main.js
+++ client/modules/core/components/tests/navbar_main.js
@@ -1,37 +1,35 @@
-/* ERROR FlowRouter is not defined */
+// TESTING DEPENDENCIES
 const {describe, it} = global;
-import {stub, spy} from 'sinon';
+import {stub} from 'sinon';
 import {expect} from 'chai';
 import {shallow} from 'enzyme';
 import proxyquire from 'proxyquire';
-proxyquire.noCallThru();
 
+// COMPONENT DEPENDENCIES
+import {Nav, Navbar, NavItem} from 'react-bootstrap';
 // stub components and helper function
 const isActiveRoute = stub();
-const Nav = () => <div></div>;
-const Navbar = () => <div></div>;
-const NavItem = () => <div></div>;
 const NavbarUser = () => <div></div>;
 
 const NavbarMain = proxyquire('../navbar_main.jsx', {
   '../components/navbar_user': NavbarUser,
   'react-bootstrap': {Nav, Navbar, NavItem},
-  '../lib/helpers.js': {isActiveRoute},
+  '../lib/helpers.js': {isActiveRoute, '@noCallThru': true},
 }).default;
 
 describe('core.components.navbar_main', () => {
   it('should contain a link to home', () => {
-    isActiveRoute.returns('active');
+    // isActiveRoute.returns('active');
     const el = shallow(<NavbarMain />);
     const homeLink = el.find('a').at(0);
     expect(homeLink.text()).to.be.equal('My Meteor Mantra');
     expect(homeLink.prop('href')).to.be.equal('/');
   });
 
-  // it('should contain a link to create a new post', () => {
-  //   const el = shallow(<NavbarMain />);
-  //   const newPostLink = el.find('a').at(1);
-  //   expect(newPostLink.text()).to.be.equal('New Post');
-  //   expect(newPostLink.prop('href')).to.be.equal('/posts');
-  // });
+  it('should contain a link to posts list', () => {
+    const el = shallow(<NavbarMain />);
+    const newPostLink = el.find('NavItem').at(0);
+    // expect(newPostLink.text()).to.be.equal('Posts');
+    expect(newPostLink.prop('href')).to.be.equal('/post');
+  });
 });
