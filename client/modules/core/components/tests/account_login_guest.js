073250c487fe462f61bd0528ca5a069bc4fbcf7e
VonIobro <jonathan.zobro@gmail.com>
2016-08-16 01:34:19 +0700

    login submit testing

--- client/modules/core/components/tests/account_login_guest.js
+++ client/modules/core/components/tests/account_login_guest.js
@@ -10,9 +10,28 @@ describe('core.components.account_login_guest', () => {
     expect(el.find('AppErrorMsg')).to.have.length(1);
   });
 
-  it('should show the login form');
+  it('should show the login form', () => {
+    const el = shallow(<AccountLoginGuest />);
+    const button = el.find('Button').first();
+    expect(button.html()).to.match(/Log In/);
+    expect(button.prop('onClick')).to.be.a('function');
+  });
 
-  it('should login when click on the submit button');
+  it('should login when click on the submit', done => {
+    const email = 'the-email';
+    const password = 'the-password';
+    const onLogin = () => {
+      expect(email).to.be.equal(email);
+      expect(password).to.be.equal(password);
+      done();
+    };
+    const el = shallow(
+      <AccountLoginGuest login={onLogin} />
+    );
+    el.setState({email, password});
+    const button = el.find('Button').first();
+    button.simulate('click');
+  });
 
   describe('if user clicks on reset password link', () => {
     it('should show the reset password modal');
