13d651bcb72af8f2fdf60ed5f540f127eec12396
VonIobro <jonathan.zobro@gmail.com>
2016-08-16 21:38:55 +0700

    test for requiremetns

--- client/modules/core/components/tests/account_password_reset_main.js
+++ client/modules/core/components/tests/account_password_reset_main.js
@@ -1,16 +1,15 @@
-const {beforeEach, describe, it} = global;
+const {describe, it} = global;
 import {expect} from 'chai';
 import {shallow} from 'enzyme';
 import AccountPasswordResetMain from '../account_password_reset_main';
 
 // if loggedIn
 describe('core.components.account_password_reset', () => {
-  it('should contain requirements'/* , () => {
+  it('should contain requirements', () => {
     const el = shallow(<AccountPasswordResetMain />);
-    AuthCheck component
-    AccountPasswordResetGuest component
-    token prop
-  }*/);
-
-  it('should contain AccountLoggedIn component');
+    console.log(el.debug())
+    expect(el.find(
+      'AccountLoggedIn', 'AccountLoggedIn').length).to.be.equal(1);
+    expect(el.props().token, 'token').to.be.defined;
+  });
 });
