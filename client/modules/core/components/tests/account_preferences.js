63b97fcae23388c19ded3aab30b5e4b4980aadd4
VonIobro <jonathan.zobro@gmail.com>
2016-08-16 22:02:10 +0700

    init test cases

--- client/modules/core/components/tests/account_preferences.js
+++ client/modules/core/components/tests/account_preferences.js
@@ -1,8 +1,16 @@
 const {beforeEach, describe, it} = global;
 import {expect} from 'chai';
 import {shallow} from 'enzyme';
+import AccountPreferences from '../account_preferences';
 
 // if loggedIn
 describe('core.components.account_preferences', () => {
-  it('should do something');
+  it('should have req props');
+  //deleteAccount
+  //user
+  it('should display user information');
+  //email
+  //username
+  it('should display delete account button')
+  it('should delete account when click on the button')
 });
