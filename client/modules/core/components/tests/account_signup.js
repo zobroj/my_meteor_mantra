e9c464ff7e4a81d3f5063c77d87418854134ef0a
VonIobro <jonathan.zobro@gmail.com>
2016-08-17 14:01:14 +0700

    test props and components

--- client/modules/core/components/tests/account_signup.js
+++ client/modules/core/components/tests/account_signup.js
@@ -1,16 +1,22 @@
+/* eslint-disable no-unused-expressions */
 const {describe, it} = global;
 import {expect} from 'chai';
 import {shallow} from 'enzyme';
+import AccountSignup from '../account_signup';
 
 // if loggedIn
 describe('core.components.account_signup', () => {
-  it('should have component imports');
-  // AuthEnsureGuest
-  // AppErrorMsg
+  it('should have component imports', () => {
+    const el = shallow(<AccountSignup />);
+    expect(el.find('UseDeps(Container(AuthEnsureGuest))')).to.have.length(1);
+    expect(el.find('Panel AppErrorMsg')).to.have.length(1);
+  });
 
-  it('should have req props');
-  // error.to.be.defined
-  // signup.to.be.defined
+  it('should have req props', () => {
+    const el = shallow(<AccountSignup />);
+    expect(el.props('error')).to.be.defined;
+    expect(el.props('signup')).to.be.defined;
+  });
 
   it('should display singup form');
   // email input state.to.be.equal('')
