260184696ed7e89378e11f8c8ee26739f96fc5e0
VonIobro <jonathan.zobro@gmail.com>
2016-08-18 03:53:30 +0700

    refactor for clarity

--- client/modules/core/components/tests/account_password_reset.js
+++ client/modules/core/components/tests/account_password_reset.js
@@ -7,19 +7,21 @@ import AccountPasswordResetGuest from '../account_password_reset';
 
 // if loggedIn
 describe('core.components.account_password_reset', () => {
-  let actions;
+  const actions = {resetPassword: stub()};
+  const token = 'the-token';
+  let button;
   let el;
-  let token;
+  let textarea1; // password1
+  let textarea2; // password2
   beforeEach(() => {
-    token = 'the-token';
-    actions = {resetPassword: stub()};
-    el = shallow(
-      <AccountPasswordResetGuest
-        token={token}
-        resetPassword={actions.resetPassword}
-      />
-    );
+    el = shallow(<AccountPasswordResetGuest
+                  token={token}
+                  resetPassword={actions.resetPassword} /> );
+    button = el.find('Button').first();
+    textarea1 = el.find({placeholder: 'Enter password'});
+    textarea2 = el.find({placeholder: 'Enter password again'});
   });
+
   it('should contain component imports', () => {
     expect(el.find('UseDeps(Container(AuthEnsureGuest))')).to.have.length(1);
     expect(el.find('AppErrorMsg', 'AppErrorMsg').length).to.be.equal(1);
@@ -27,9 +29,6 @@ describe('core.components.account_password_reset', () => {
 
   it('should show reset password form', () => {
     expect(el.find('form#password-reset').length, 'form').to.be.equal(1);
-    const textarea1 = el.find({placeholder: 'Enter password'});
-    const textarea2 = el.find({placeholder: 'Enter password again'});
-    const button = el.find('Button').first();
     expect(textarea1.prop('value')).to.be.equal('');
     expect(textarea2.prop('value')).to.be.equal('');
     expect(button.prop('onClick')).to.be.a('function');
@@ -38,19 +37,17 @@ describe('core.components.account_password_reset', () => {
   it('should handle text change', () => {
     const newText1 = 'hello1';
     const newText2 = 'hello2';
-    const textarea1 = el.find({placeholder: 'Enter password'});
-    const textarea2 = el.find({placeholder: 'Enter password again'});
     textarea1.simulate('change', {target: {value: newText1}});
     expect(el.state('password1'), 'password1').to.be.equal(newText1);
     textarea2.simulate('change', {target: {value: newText2}});
     expect(el.state('password2'), 'password2').to.be.equal(newText2);
   });
 
-  it('should resetPassword when click button', () => {
+  it('should call resetPassword when click button', () => {
     const password1 = 'the-password1';
     const password2 = 'the-password2';
     el.setState({password1, password2});
-    el.find('form#password-reset Button').simulate('click');
+    button.simulate('click');
     const args = actions.resetPassword.args[0];
     expect(args.slice(0,3)).to.deep.equal([ token, password1, password2 ]);
   });
