52ed8472429b10fcd833d34194e5252f284c1fd2
VonIobro <jonathan.zobro@gmail.com>
2016-08-18 02:49:13 +0700

    streamline tests

--- client/modules/comments/components/tests/comment_list.js
+++ client/modules/comments/components/tests/comment_list.js
@@ -1,24 +1,26 @@
-const {describe, it} = global;
+const {beforeEach, describe, it} = global;
 import {expect} from 'chai';
 import { shallow } from 'enzyme';
 import CommentList from '../comment_list';
 
 describe('comments.components.comment_list', () => {
+  let el; let divs;
   const comments = [
     {_id: 'one', username: 'u-1', text: 't-1', createdAt: new Date(), saving: true },
     {_id: 'two', username: 'u-2', text: 't-2', createdAt: new Date(), saving: false },
     {_id: 'three', username: 'u-3', text: 't-3', createdAt: new Date(), saving: true },
     {_id: 'four', username: 'u-4', text: 't-4', createdAt: new Date(), saving: false }
   ];
+  beforeEach(() => {
+    el = shallow(<CommentList comments={comments}/>);
+    divs = el.find('.comment');
+  });
 
   it('should list given number of items', () => {
-    const el = shallow(<CommentList comments={comments}/>);
-    expect(el.find('.comment').length).to.be.equal(comments.length);
+    expect(divs.length).to.be.equal(comments.length);
   });
 
   it('should list comment author for each item', () => {
-    const el = shallow(<CommentList comments={comments}/>);
-    const divs = el.find('.comment');
     divs.forEach((div, i) => {
       const username = div.find('strong').first().text();
       expect(username).to.be.equal(comments[i].username);
@@ -26,8 +28,6 @@ describe('comments.components.comment_list', () => {
   });
 
   it('should list comment text for each item', () => {
-    const el = shallow(<CommentList comments={comments}/>);
-    const divs = el.find('.comment');
     divs.forEach((div, i) => {
       const text = div.find('p').first().text();
       expect(text).to.have.string(comments[i].text);
@@ -35,8 +35,6 @@ describe('comments.components.comment_list', () => {
   });
 
   it('should list saving status for each item', () => {
-    const el = shallow(<CommentList comments={comments}/>);
-    const divs = el.find('.comment');
     divs.forEach((div, i) => {
       const text = div.html();
       if (comments[i].saving) {
