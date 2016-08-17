eb94e9d07310f7eb88818bae6c9a5d012e267778
VonIobro <jonathan.zobro@gmail.com>
2016-08-18 01:37:37 +0700

    test for onchange

--- client/modules/comments/components/tests/comment_create.js
+++ client/modules/comments/components/tests/comment_create.js
@@ -5,19 +5,26 @@ import CommentCreate from '../comment_create';
 
 describe('comments.components.comment_create', () => {
   it('contains import components', () => {
-    const wrapper = shallow(<CommentCreate />);
-    expect(wrapper.find('UseDeps(Container(AuthEnsureUser))'))
+    const el = shallow(<CommentCreate />);
+    expect(el.find('UseDeps(Container(AuthEnsureUser))'))
       .to.have.length(1);
-    expect(wrapper.find('AppErrorMsg')).to.have.length(1);
+    expect(el.find('AppErrorMsg')).to.have.length(1);
   });
 
   it('should display the create comment form', () => {
-    const wrapper = shallow(<CommentCreate />);
-    const textarea = wrapper.find('FormControl').first();
-    const button = wrapper.find('Button').first();
-
+    const el = shallow(<CommentCreate />);
+    const textarea = el.find('FormControl').first();
+    const button = el.find('Button').first();
     expect(textarea.prop('value')).to.be.equal('');
     expect(button.prop('onClick')).to.be.a('function');
+  });
+
+  it('should update text-state when text changed', () => {
+    const newText = 'hello';
+    const el = shallow(<CommentCreate />);
+    const textarea = el.find('FormControl').first();
+    textarea.simulate('change', {target: {value: newText}});
+    expect(el.state('comment')).to.be.equal(newText);
   });
 
   it('should create a new comment when click on the button', done => {
