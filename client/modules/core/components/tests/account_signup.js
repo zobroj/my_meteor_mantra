09bd72e77de210564800d8f4e1176c0e199c7a9f
VonIobro <jonathan.zobro@gmail.com>
2016-08-18 21:10:55 +0700

    clean up test

--- client/modules/core/components/tests/account_signup.js
+++ client/modules/core/components/tests/account_signup.js
@@ -1,30 +1,29 @@
-/* eslint-disable no-unused-expressions */
-const {describe, it} = global;
+const {beforeEach, describe, it} = global;
 import {expect} from 'chai';
 import {shallow} from 'enzyme';
+import {stub} from 'sinon';
 import AccountSignup from '../account_signup';
 
-// if loggedIn
 describe('core.components.account_signup', () => {
+  var actions;
+  var button;
+  var el;
+  beforeEach(() => {
+    actions = {signup: stub()};
+    el = shallow(<AccountSignup signup={actions.signup}/>);
+    button = el.find('Button.submit');
+  });
+
   it('should have component imports', () => {
-    const el = shallow(<AccountSignup />);
     expect(el.find('UseDeps(Container(AuthEnsureGuest))')).to.have.length(1);
     expect(el.find('Panel AppErrorMsg')).to.have.length(1);
   });
 
-  it('should have req props', () => {
-    const el = shallow(<AccountSignup />);
-    expect(el.props('error')).to.be.defined;
-    expect(el.props('signup')).to.be.defined;
-  });
-
-  it('should display singup form', () => {
-    const el = shallow(<AccountSignup />);
+  it('should render signup form', () => {
     const emailInput = el.find({type: 'email'});
     const usernameInput = el.find({type: 'text'});
     const password1Input = el.find({placeholder: 'Enter password'});
     const password2Input = el.find({placeholder: 'Confirm password'});
-    const button = el.find('Button.submit');
     expect(emailInput).to.have.length(1);
     expect(emailInput.prop('value')).to.be.equal('');
     expect(usernameInput).to.have.length(1);
@@ -37,24 +36,15 @@ describe('core.components.account_signup', () => {
     expect(button.prop('onClick')).to.be.a('function');
   });
 
-  it('should signup user when button click', done => {
+  it('should signup user when button click', () => {
     const email = 'the-email';
     const username = 'the-username';
     const password1 = 'the-password1';
     const password2 = 'the-password2';
-    const onClick = () => {
-      /* TODO not very effective as this will always pass */
-      expect(username).to.be.equal(username);
-      expect(password1).to.be.equal(password1);
-      expect(password2).to.be.equal(password2);
-      done();
-    };
-    const el = shallow(<AccountSignup signup={onClick}/>);
     el.setState({email});
     el.setState({username});
     el.setState({password1});
     el.setState({password2});
-    const button = el.find('Button.submit');
     button.simulate('click');
   });
 });
