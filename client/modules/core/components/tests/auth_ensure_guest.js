3234c572ccbe4832cf2ab2045c15000a2b0a0779
VonIobro <jonathan.zobro@gmail.com>
2016-08-20 01:17:31 +0700

    test authEnsureGuest

--- client/modules/core/components/tests/auth_ensure_guest.js
+++ client/modules/core/components/tests/auth_ensure_guest.js
@@ -1,8 +1,33 @@
-const {beforeEach, describe, it} = global;
+const {describe, it} = global;
 import {expect} from 'chai';
-import {shallow} from 'enzyme';
+import {mount} from 'enzyme';
+import AuthEnsureGuest from '../auth_ensure_guest';
 
 // if loggedIn
 describe('core.components.auth_ensure_guest', () => {
-  it('should do something');
+  describe('if user is loggingIn', () => {
+    it('should display the loading message', () => {
+      const el = mount(<AuthEnsureGuest loggingIn={true}/>);
+      const div = el.find('div').first();
+      expect(div.html()).to.match(/Loading/);
+    });
+  });
+
+  describe('if user is loggedIn', () => {
+    it('should display AccountLoggedIn component', () => {
+      const el = mount(<AuthEnsureGuest loggedIn={true}/>);
+      expect(el.find('AccountLoggedIn').length).to.be.equal(1);
+    });
+  });
+
+  describe('if user is guest (!loggedIn)', () => {
+    it('should display the imported child elements', () => {
+      const dummyChildren = 'DummyChildren';
+      const el = mount(<AuthEnsureGuest
+                        loggedIn={false}
+                        children={dummyChildren} />);
+      const div = el.find('div').first();
+      expect(div.html()).to.match(/DummyChildren/);
+    });
+  });
 });
