a96a61c42bdb68317daa5a5dffc00011ef262733
VonIobro <jonathan.zobro@gmail.com>
2016-08-20 01:58:27 +0700

    test ensure user

--- client/modules/core/components/tests/auth_ensure_user.js
+++ client/modules/core/components/tests/auth_ensure_user.js
@@ -1,8 +1,43 @@
-const {beforeEach, describe, it} = global;
+const {describe, it} = global;
 import {expect} from 'chai';
-import {shallow} from 'enzyme';
+import {mount} from 'enzyme';
+import AuthEnsureUser from '../auth_ensure_user';
 
 // if loggedIn
 describe('core.components.auth_ensure_user', () => {
-  it('should do something');
+  describe('if user is loggingIn', () => {
+    it('should display loadingComponent', () => {
+      const el = mount(<AuthEnsureUser loggingIn={true}/>);
+      const div = el.find('div').first();
+      expect(div.html()).to.match(/Loading/);
+    });
+  });
+
+  describe('if user is loggedIn and emailVerified', () => {
+    it('should display children', () => {
+      const dummyChildren = 'DummyChildren';
+      const el = mount(<AuthEnsureUser
+                        loggedIn={true}
+                        emailVerified={true}
+                        children={dummyChildren} />);
+      const div = el.find('div').first();
+      expect(div.html()).to.match(/DummyChildren/);
+    });
+  });
+  describe('if user is loggedIn and !emailVerified', () => {
+    it('should display unverifiedComponent', () => {
+      const el = mount(<AuthEnsureUser
+                        loggedIn={true}
+                        emailVerified={false} />);
+      const div = el.find('div').first();
+      expect(div.html()).to.match(/unverified users/);
+    });
+  });
+  describe('if guest', () => {
+    it('should display guestComponent', () => {
+      const el = mount(<AuthEnsureUser loggedIn={false}/>);
+      const div = el.find('div').first();
+      expect(div.html()).to.match(/Please log in/);
+    });
+  });
 });
