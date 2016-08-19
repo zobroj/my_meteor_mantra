98093d2f69830676cfb9095142e583db3b58e6a9
VonIobro <jonathan.zobro@gmail.com>
2016-08-20 02:28:46 +0700

    test layoutlist

--- client/modules/core/components/tests/layout_list.js
+++ client/modules/core/components/tests/layout_list.js
@@ -1,8 +1,21 @@
-const {beforeEach, describe, it} = global;
+const {describe, it} = global;
 import {expect} from 'chai';
 import {shallow} from 'enzyme';
+import LayoutList from '../layout_list';
+import { AppVerifiedMsg, NavbarMain } from '/client/configs/components';
 
-// if loggedIn
 describe('core.components.layout_list', () => {
-  it('should do something');
+  it('should contain component imports', () => {
+    const el = shallow(<LayoutList />);
+    expect(el.contains(<NavbarMain/>)).to.be.equal(true);
+    expect(el.contains(<AppVerifiedMsg/>)).to.be.equal(true);
+  });
+
+  it('should render children', () => {
+    const Comp = () => (<p>Hello</p>);
+    const el = shallow(
+      <LayoutList content={() => (<Comp />)}/>
+    );
+    expect(el.contains(<Comp />)).to.be.equal(true);
+  });
 });
