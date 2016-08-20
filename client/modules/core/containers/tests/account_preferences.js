c700b0091d78b6adefa699973e546399b83f64fb
VonIobro <jonathan.zobro@gmail.com>
2016-08-20 22:50:31 +0700

    test account preferences  container

---
+++ client/modules/core/containers/tests/account_preferences.js
@@ -0,0 +1,32 @@
+const {beforeEach, describe, it} = global;
+import {expect} from 'chai';
+import {spy} from 'sinon';
+import {depsMapper} from '../account_preferences';
+
+describe('comments.containers.account_password_reset', () => {
+  describe('depsMapper', () => {
+    var actions;
+    var context;
+    var deps;
+    beforeEach(() => {
+      actions = {accounts: {
+        clearErrors: spy(), deleteAccount: spy()
+      }};
+      context = spy();
+      deps = depsMapper(context, actions);
+    });
+
+    describe('actions', () => {
+      it('should map deps', () => {
+        expect(deps.clearErrors).to.be.equal(actions.accounts.clearErrors);
+        expect(deps.deleteAccount).to.be.equal(actions.accounts.deleteAccount);
+      });
+    });
+
+    describe('context', () => {
+      it('should map the whole context as a function', () => {
+        expect(deps.context()).to.be.equal(context);
+      });
+    });
+  });
+});
