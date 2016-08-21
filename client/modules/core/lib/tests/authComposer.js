0466dffef90f91f57084cea8b764e5ba35eb95ab
VonIobro <jonathan.zobro@gmail.com>
2016-08-22 02:22:29 +0700

    init authComposer testing

--- client/modules/core/lib/tests/authComposer.js
+++ client/modules/core/lib/tests/authComposer.js
@@ -1,8 +1,47 @@
 const {afterEach, beforeEach, describe, it} = global;
 import {expect} from 'chai';
 import {spy, stub} from 'sinon';
-// import actions from '../accounts';
+import authComposer from '../authComposer';
 
 describe('core.lib.authComposer', () => {
-  it('should test something');
+  const USER = {
+    id: 'the-id',
+    email: 'the-email',
+    profile: 'the-profile',
+    username: 'the-username',
+  };
+  const emailVerified = true;
+  const loggedIn = true;
+  const loggingIn = true;
+  var Meteor; var context; var onData;
+  beforeEach(() => {
+    Meteor = {userId: spy(), subscribe: spy(), user: spy(), loggingIn: spy()};
+    onData = () => spy();
+    context = {Meteor};
+  });
+
+  it('should gather user information', () => {
+    authComposer({context}, onData);
+    // id
+    // email
+    // profile
+    // username
+  });
+
+  it('should publish user user information to onData', () => {
+    // emailVerified
+    // loggedIn
+    // loggingIn
+    // user
+  });
+
+  describe('should subscribe to users.current', () => {
+    describe('before subscribed', () => {
+      it('should return null');
+    });
+
+    describe('after subscribed', () => {
+      it('should return userId');
+    });
+  });
 });
