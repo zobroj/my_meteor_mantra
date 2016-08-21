ebf9a7cb4b7e20c3a37b6e96274bb653384e47fe
VonIobro <jonathan.zobro@gmail.com>
2016-08-22 01:39:44 +0700

    tests post_list

--- client/modules/posts/containers/post_list.js
+++ client/modules/posts/containers/post_list.js
@@ -1,22 +1,21 @@
 import PostList from '../components/post_list.jsx';
-import { useDeps, composeWithTracker, composeAll } from 'mantra-core';
+import {useDeps, composeWithTracker, composeAll} from 'mantra-core';
 
-export const composer = ({ context, clearErrors }, onData) => {
-  const { Meteor, Collections, LocalState } = context();
-  const errorState = 'POSTS_ERROR';
-  const error = LocalState.get(errorState);
+export const composer = ({context, clearErrors}, onData) => {
+  const {Collections, LocalState, Meteor} = context();
+  const error = LocalState.get('POSTS_ERROR');
   const subscription = Meteor.subscribe('posts.list');
 
   if (subscription.ready()) {
-    const options = {
-      sort: { createdAt: -1 },
-    };
+    const options = {sort: { createdAt: -1 }};
     const posts = Collections.Posts.find({}, options).fetch();
 
     onData(null, { posts, error });
   }
-
-  return clearErrors.bind(errorState);
+  const cleanup = () => {
+    clearErrors('POSTS_ERROR');
+  };
+  return cleanup;
 };
 
 export const depsMapper = (context, actions) => ({
--- client/modules/posts/containers/tests/post_list.js
+++ client/modules/posts/containers/tests/post_list.js
@@ -1,40 +1,80 @@
-/*
-const { describe, it } = global;
+const {beforeEach, describe, it} = global;
 import {expect} from 'chai';
 import {stub, spy} from 'sinon';
-import {composer} from '../post_list';
+import {composer, depsMapper} from '../post_list';
 
-describe('core.containers.post_list', () => {
+describe('posts.containers.post_list', () => {
   describe('composer', () => {
-    it('should subscribe to posts.list', () => {
-      const Meteor = {subscribe: stub()};
+    const posts = [ {_id: 'aa'} ];
+    const Collections = {Posts: {find: stub()}};
+    var LocalState;
+    var Meteor;
+    var context;
+    var onData;
+    beforeEach(() => {
+      Collections.Posts.find.returns({fetch: () => posts});
+      LocalState = {get: spy()};
+      Meteor = {subscribe: stub()};
       Meteor.subscribe.returns({ready: () => false});
+      context = () => ({Collections, LocalState, Meteor});
+      onData = spy();
+    });
 
-      const context = () => ({Meteor});
-      const onData = spy();
+    it('should get POSTS_ERROR from LocalState', () => {
+      composer({context}, spy());
+      const args = LocalState.get.args[0];
+      expect(args).to.have.length(1);
+      expect(args[0]).to.be.equal('POSTS_ERROR');
+    });
 
+    it('should subscribe to posts.list', () => {
       composer({context}, onData);
       expect(Meteor.subscribe.args[0]).to.deep.equal([
         'posts.list'
       ]);
     });
 
+    it('should return cleanup func', () => {
+      const clearErrors = spy();
+      const clearFunc = composer({context, clearErrors}, spy());
+      expect(clearFunc.name).to.be.deep.equal('cleanup');
+    });
+
     describe('after subscribed', () => {
-      it('should fetch data from all posts & pass to onData', () => {
-        const Meteor = {subscribe: stub()};
+      it('should fetch data "error" and posts and pass to onData', () => {
+        LocalState = {get: stub().returns('error')};
         Meteor.subscribe.returns({ready: () => true});
 
-        const posts = [ {_id: 'aa'} ];
-        const Collections = {Posts: {find: stub()}};
-        Collections.Posts.find.returns({fetch: () => posts});
-
-        const context = () => ({Meteor, Collections});
-        const onData = spy();
-
         composer({context}, onData);
-        expect(onData.args[0]).to.deep.equal([ null, {posts} ]);
+        const args = onData.args[0];
+        expect(args[0]).to.be.equal(null);
+        expect(args[1]).to.be.deep.equal({
+          error: 'error', posts
+        });
+      });
+    });
+  });
+
+  describe('depsMapper', () => {
+    var actions; var context; var deps;
+    beforeEach(() => {
+      actions = {posts: {
+        clearErrors: spy()
+      }};
+      context = spy();
+      deps = depsMapper(context, actions);
+    });
+
+    describe('actions', () => {
+      it('should map deps', () => {
+        expect(deps.clearErrors).to.be.equal(actions.posts.clearErrors);
+      });
+    });
+
+    describe('context', () => {
+      it('should map the whole context as a function', () => {
+        expect(deps.context()).to.be.equal(context);
       });
     });
   });
 });
-*/
