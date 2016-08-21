eb6827295ae9be4ebebb01c7a34350006772870b
VonIobro <jonathan.zobro@gmail.com>
2016-08-21 17:03:57 +0700

    check state changes

--- client/modules/posts/components/tests/post_create.js
+++ client/modules/posts/components/tests/post_create.js
@@ -28,11 +28,17 @@ describe('posts.components.post_create', () => {
     expect(el.find('AppErrorMsg')).to.have.length(1);
   });
 
-
   it('should display the create post form', () => {
     expect(titleInput.prop('value')).to.be.equal('');
     expect(textInput.prop('value')).to.be.equal('');
     expect(button.prop('onClick'), 'button').to.be.a('function');
+  });
+
+  it('should handle text-state changes', () => {
+    titleInput.simulate('change', {target: {value: title}});
+    expect(el.state('title'), 'title').to.be.equal(title);
+    textInput.simulate('change', {target: {value: text}});
+    expect(el.state('text'), 'text').to.be.equal(text);
   });
 
 /*
